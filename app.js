0b70b758becfad129578d69d5c2d0665e50b1b74
preethi-muthukumaran <53194052+preethi-muthukumaran@users.noreply.github.com>
2020-11-15 22:56:54 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+
+import Recipe from './Recipe';
+
+
+
+function App() {
+  return (
+    <div>
+      {/* <Recipe name = 'Pizza' url = 'pizza.json'/> */}
+      <Recipe />
+    </div>
+   
+  );
+}
+
+export default App;
